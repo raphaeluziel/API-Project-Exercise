9120565ae8ecd70a9aa2962e46c2dda0e6dc70ee
Glitch (api-uziel-exercise) <none>
2019-01-09 15:49:31 +0000

    🏜🌋 Updated with Glitch

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
-
 const cors = require('cors')
 
 var port = process.env.PORT || 3000;
@@ -11,18 +10,32 @@ var port = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_URI, { useMongoClient: true });
 
 app.use(cors())
-
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-
-
 app.use(express.static('/'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/')
 });
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+var Schema = mongoose.Schema;
 
+var userSchema = new Schema({
+  username: {type: String, required: true},
+  //userId: {type: String, required: true}
+});
+
+var userModel = mongoose.model('userModel', userSchema);
+
+app.route('/api/exercise/new-user').post(function(req, res){
+
+  res.json({"message": req.body.username});
+
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
